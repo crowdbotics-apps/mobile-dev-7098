1d6d2b804e12af0a137d8d4dac731a70a1ae321d
Crowdbotics <team@crowdbotics.com>
2020-07-06 08:53:01 +0000

    Update components

--- src/features/BlankScreen18008/screens/blank.js
+++ src/features/BlankScreen18008/screens/blank.js
@@ -18,13 +18,7 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = {
-    Toggle_2: true,
-    CheckBox_4: true,
-    Radio_5: true,
-    Input_7: "",
-    Input_8: ""
-  }
+  state = { Toggle_2: true, CheckBox_4: true, Radio_5: true, Input_8: "" }
 
   render = () => (
     <View
@@ -274,42 +268,6 @@ export class _Blank extends React.Component {
           fontWeight: "normal",
           fontFamily: "System"
         }}
-      />
-      <Input
-        placeholder="Number Input Placeholder"
-        editable={true}
-        keyboardType="numeric"
-        textStyle={{ fontSize: 20, color: "#000000" }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 20,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontStyle: "normal",
-          fontWeight: "normal",
-          fontFamily: "System",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0
-        }}
-        value={this.state.Input_7}
-        onChangeText={nextValue => this.setState({ Input_7: nextValue })}
       />
       <Input
         placeholder="Sample text input placeholder"
